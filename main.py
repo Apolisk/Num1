6e95145907a54db18476b58dce766ba1d6d2df72
Apolisk <63949026+Apolisk@users.noreply.github.com>
2021-08-23 14:21:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+bot = Bot(token="1807080422:AAG-Gf9brQuehFW28UbMrTDQY04KjoHbOvU")
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+scheduler = AsyncIOScheduler()
+
+
+async def create_poll(dp: Dispatcher):
+    await dp.bot.send_poll(chat_id=-1001575536302, question='Today:', options=['6', '7'], type='regular',
+                           is_anonymous=False)
+
+
+def schedule_jobs():
+    scheduler.add_job(create_poll, "cron", day_of_week='mon-fri', hour=, minute=5, args=(dp,))
+
+async def on_startup(dp):
+    schedule_jobs()
+
+if __name__ == '__main__':
+    scheduler.start()
+    executor.start_polling(dp, on_startup=on_startup)
+
